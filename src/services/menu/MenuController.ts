8c4ffba1225cd97eb30bfc29d0d3d0d34dcbbef4
raminarmanfar <ramin.armanfar@msg.group>
2018-11-08 12:05:12 +0100

    Menu items for admin dashboard and user dashboard fixed

--- src/services/menu/MenuController.ts
+++ src/services/menu/MenuController.ts
@@ -8,9 +8,9 @@ export default class MenuController {
         return new Promise((resolve: any) => {
             const menuList: Array<MenuItem> = new Array<MenuItem>();
             menuList.push(new MenuItem('Home', '/', new Array<UserRoleEnum>(UserRoleEnum.Public, UserRoleEnum.User, UserRoleEnum.Admin), 'Home page'));
-            menuList.push(new MenuItem('Dashboard', '/dashboard', new Array<UserRoleEnum>(UserRoleEnum.User), 'Your personal dashboard'));
-            menuList.push(new MenuItem('Dashboard', '/admin-dashboard', new Array<UserRoleEnum>(UserRoleEnum.Admin), 'Administrator dashboard'));
-            menuList.push(new MenuItem('Manage Users', '/admin-dashboard/manage-users', new Array<UserRoleEnum>(UserRoleEnum.Admin), 'My projects'));
+            menuList.push(new MenuItem('Dashboard', '/dashboard/user-page', new Array<UserRoleEnum>(UserRoleEnum.User), 'Your personal dashboard'));
+            menuList.push(new MenuItem('Dashboard', '/dashboard/admin-page', new Array<UserRoleEnum>(UserRoleEnum.Admin), 'Administrator dashboard'));
+            menuList.push(new MenuItem('Manage Users', '/dashboard/manage-users', new Array<UserRoleEnum>(UserRoleEnum.Admin), 'My projects'));
             menuList.push(new MenuItem('Projects', '/public/projects', new Array<UserRoleEnum>(UserRoleEnum.Public, UserRoleEnum.User, UserRoleEnum.Admin), 'My projects'));
             menuList.push(new MenuItem('Goals', '/public/goals', new Array<UserRoleEnum>(UserRoleEnum.Public, UserRoleEnum.User, UserRoleEnum.Admin), 'My Goals'));
             menuList.push(new MenuItem('About Me', '/public/about-me', new Array<UserRoleEnum>(UserRoleEnum.Public, UserRoleEnum.User, UserRoleEnum.Admin), 'Breifly about my life'));
