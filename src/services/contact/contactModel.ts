b105f9525441128c8eaef7c0583509b50d817a28
raminarmanfar <ramin.armanfar@msg.group>
2018-11-08 14:00:14 +0100

    contact module code style improved

--- src/services/contact/contactModel.ts
+++ src/services/contact/contactModel.ts
@@ -1,35 +1,12 @@
 import { model, Schema } from 'mongoose';
-
 // tslint:disable object-literal-sort-keys
 const ContactSchema: Schema = new Schema({
-  createdAt: {
-    type: Date,
-    default: Date.now
-  },
-  updatedAt: {
-    type: Date,
-    default: Date.now
-  },
-  firstName: {
-    type: String,
-    default: '',
-    required: true
-  },
-  lastName: {
-    type: String,
-    default: '',
-    required: true
-  },
-  email: {
-    type: String,
-    default: '',
-    required: true
-  },
-  subject: {
-    type: String,
-    default: '',
-    required: true
-  }
+  createdAt: { type: Date, default: Date.now },
+  updatedAt: { type: Date, default: Date.now },
+  firstName: { type: String, default: '', required: true },
+  lastName: { type: String, default: '', required: true },
+  email: { type: String, default: '', required: true },
+  subject: { type: String, default: '', required: true }
 });
 
 export default model('Contact', ContactSchema);
